f350798cde509a22e0e0ddd0a61ee3cc8e5b080f
Yesid Varela <yleandrovarela@gmail.com>
2021-11-02 13:22:27 -0500

    Start server express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+let initial_path = path.join(__dirname, "public");
+
+let app = express();
+app.use(express.static(initial_path));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(initial_path, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("--------------------------");
+  console.log("Servidor en el puerto 3000");
+  console.log("--------------------------");
+});
