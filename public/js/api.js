14a74688402ab6e80d06d2bdd38264e32658398e
Yesid Varela <yleandrovarela@gmail.com>
2021-11-02 19:22:40 -0500

    Call Api, insert img and title in html it works

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+const api_key = "fca026edba55620a077ed3bc436bfe11";
+const img_url = "https://image.tmdb.org/t/p/w500";
+const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+const movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
